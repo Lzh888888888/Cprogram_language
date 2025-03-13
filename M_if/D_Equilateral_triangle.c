fe97115c7b0975b38cd8cd513d31c0788e6ee1a1
Lzh <luzonghan0808@gmail.com>
2025-03-13 20:01:24 +0800

    20250313

---
+++ M_if/D_Equilateral_triangle.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+/*
+06-05 06-06 06-07 06-08 06-09
+正三角形、等腰三角形、直角三角形判斷練習+綜合+簡化(使用if)
+*/
+
+int main()
+{
+    int side1,side2,side3;
+
+    printf("Please enter the lengths:");
+    scanf("%d%d%d",&side1,&side2,&side3);
+
+    if(side1 == side2 && side2 == side3){
+        printf("Regular triangle\n");//正三角形
+    }
+    if(side1 == side2 || side2 == side3 || side1 == side3){
+        printf("Isosceles treangle\n");//等腰三角形
+    }
+    if(side1 * side1 +side2 * side2== side3 * side3 ||
+        side2 * side2 + side3 * side3 == side1 * side1 || 
+        side1 * side1 + side3 * side3 == side2 * side2 ){//平方不能使用^2
+        printf("Rectangular triangle\n");//直角三角形
+    }
+
+    //簡化版(指定side1 <= side2 <= side3)
+    if(side1 <= side2 <= side3){
+        if(side1 == side3){
+            printf("Regular triangle\n");//正三角形
+        }
+        if(side1 == side2 || side2 == side3){
+            printf("Isosceles treangle\n");//等腰三角形
+        }
+        if(side1 * side1 +side2 * side2== side3 * side3){
+            printf("Rectangular triangle\n");//直角三角形
+        }
+    }
+
+    return 0;
+}
